46a54e105b3e93170e9a1ee6c7fcde4b98a2b7c6
Enoc M <magallon.estrada.e@gmail.com>
2022-10-30 11:59:06 -0600

    Programa calcula el mayor

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const x = 1;
+var prompt = require('prompt-sync')();
+//Leer el primer numero
+const n1 = parseInt(prompt("Escribe el numero 1: "));
+//Leer el segundo numero
+const n2 = parseInt(prompt("Escribe el numero 2: "));
 
-const nombre = "Samuel";
+const n3 = parseInt(prompt("Escribe el numero 3: "));
+
+//Validar el mayor
+
+let mayor = 0;
+
+if (n1 > n2){
+    mayor = n1;
+} else {
+    mayor = n2;
+} //else if (n3 > mayor){
+//     mayor = n3;
+// }
+
+if(n3 > mayor){
+    mayor = n3;
+}
+
+console.log('El numero ' + mayor + ' es mayor');

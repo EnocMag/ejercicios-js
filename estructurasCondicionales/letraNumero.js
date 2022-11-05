8e334434b39feea6e4b2163b4955d4b47282ea04
Enoc M <magallon.estrada.e@gmail.com>
2022-11-04 20:22:51 -0600

    app Letra o Número
    app pendiente aun no terminado

---
+++ estructurasCondicionales/letraNumero.js
@@ -0,0 +1,12 @@
+var prompt = require('prompt-sync')();
+
+// var a = parseInt(prompt('Ingrese caracter: ')) ;
+// var boleano = true;
+// console.log('El tipo de dato es: ' + typeof boleano);
+// //console.log(typeof a);
+
+// console.log(typeof(a));
+// if (){
+//     console.log('Es letra');
+// }
+
